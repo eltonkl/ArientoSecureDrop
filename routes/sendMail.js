3b7e10f88cc428ea68e0b69930e5cbb790db97f6
eltonkl <eltonkl@users.noreply.github.com>
2017-04-23 15:40:33 -0700

    Add sendMail route

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* POST send mail */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ariento Secure Drop' });
+  res.send('respond with a resource');
 });
 
 module.exports = router;
