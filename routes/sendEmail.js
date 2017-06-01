42e3a45aec0e8404addea1964387e0353882dab8
Darthpwner <matthewallenlin@gmail.com>
2017-06-01 00:10:37 -0700

    Changed colors of button and file uploader so the text is visible

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -24,14 +24,14 @@ fs.readdir(upload, function(err, files){
   if(err)
     console.log("err");
       
-  files.forEach(function(file, index){ 
-    var fileObject =  {
-      filename: file,
-      path: upload + "/" + file,
-      cid: file
-    }
-    attachFiles.push(fileObject);
-  });
+  // files.forEach(function(file, index){ 
+  //   var fileObject =  {
+  //     filename: file,
+  //     path: upload + "/" + file,
+  //     cid: file
+  //   }
+  //   attachFiles.push(fileObject);
+  // });
 });
 
 /* POST send mail */
